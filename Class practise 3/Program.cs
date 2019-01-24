0896303a44bd5e85d1812ea328acdbf8a6b91a95
visualstudio <visualstudio@230-PC27>
2019-01-24 15:20:20 -0500

    practice

--- Class practise 3/Program.cs
+++ Class practise 3/Program.cs
@@ -10,8 +10,29 @@ namespace Class_practise_3
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine(1+1);
+            var dog_and_cat = new car();
+            var bowling = new car();
+            Console.WriteLine("How many cars: {0}", Vehicle.howManyCars);
+            Console.ReadLine();   
         }
+    }
+
+    class Vehicle
+    {
+
+        public static int howManyCars=0;
+        public String color = "Blue";
+    }
+
+    class car : Vehicle
+    {
+          public String Color = "Red";
+          public car()
+        {
+            Vehicle.howManyCars++;
+            Console.WriteLine("my color is " + color);
+            Console.ReadLine();
+
+       }
     }
 }
